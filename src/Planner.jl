cc36bc48987478ad6b5e68f9f72d809813949896
spbost <sorenpost@pm.me>
2021-10-22 17:37:08 +0200

    Bunch of changes. Is working now.

--- src/Planner.jl
+++ src/Planner.jl
@@ -3,111 +3,183 @@ module Planner
 using Graphs
 using DataFrames
 
-"Add a inputs->outputs combination to the dependency graph."
+mutable struct Plan
+    G::SimpleDiGraph
+    graph_data::DataFrame
+end
+
 """
-    add_taget!(G, graph_data, ["output1.txt"], ["input1.txt", "input2.txt"])
+    a_plan = startplanning()
+
+Initialise a Plan to track the project dependencies (inputs -> outputs
+ relationships). The functiion simply outputs an empty Plan-object with a graph
+ G, and an associated dataframe, graph_data.
+"""
+function startplanning()
+    G = SimpleDiGraph()
+    graph_data = DataFrame(index = Int[], filename = String[])
+    return Plan(G, graph_data)
+end
+
+"""
+    addtarget!(plan, outputs, inputs)
 
 Add a element to the plan (inputs -> output) defined by `G` and `graph_data`.
 
 # Arguments
-* `G`: The graph-object that represent the dependency graph.
-* `graph_data`: The dataframe that contains the information associated with nodes in `G`
+* `plan`: The plan (::Plan) that represents the dependency graph. The input-
+output combination is added to this plan.
 * `outputs`: The files that results from the inputs (scripts, data).
-* `depends_on`: The files that creates the outputs. The outpus must be created by running
-the .jl scripts in `depends_on`.
+* `depends_on`: The files that creates the outputs. The outpus must be created
+by running the .jl scripts in `depends_on`.
 
+# Examples
+```julia
+new_plan = startplanning()
+addtarget!(new_plan,
+           ["files/someoutput.csv"],
+           ["files/someinputdata.txt", "somescript.jl"])
+```
 """
-function add_target!(G::SimpleDiGraph{Int64}, graph_data::DataFrame, outputs::Array{String}, depends_on::Array{String})
+function addtarget!(plan::Plan, outputs::Array{String}, depends_on::Array{String})
     # Check if inputs, outputs are already in graph:
     # If yes, just add edges. If no, first add as vertices.
-    not_in_graph = [file for file in vcat(depends_on, outputs) if !has_node(graph_data, file)]
-    length(not_in_graph) > 0 ? add_node!.(Ref(G), Ref(graph_data), not_in_graph) : nothing
+    not_in_graph = [file for file in vcat(depends_on, outputs) if !hasnode(plan, file)]
+    length(not_in_graph) > 0 ? addnode!.(Ref(plan), not_in_graph) : nothing
 
     # Add edges:
-    input_indices = filter(row -> row.filename ∈ depends_on, graph_data).index
-    output_indices = filter(row -> row.filename ∈ outputs, graph_data).index
+    input_indices = filter(row -> row.filename ∈ depends_on,
+                           plan.graph_data).index
+    output_indices = filter(row -> row.filename ∈ outputs,
+                            plan.graph_data).index
     for i in input_indices
         for o in output_indices
             # Does edge exit?
-            has_edge(G, i, o) ? nothing : add_edge!(G, i, o)
+            has_edge(plan.G, i, o) ? nothing : add_edge!(plan.G, i, o)
         end
     end
 end
 
-"Check if the dependency graph has a node with the given filename attached."
-function has_node(graph_data::DataFrame, filename::String)
-    return filename ∈ graph_data.filename
+"""
+    hasnode(plan::Plan, filename::String)
+
+Check if the dependency graph has a node with the given filename attached.
+"""
+function hasnode(plan::Plan, filename::String)
+    return filename ∈ plan.graph_data.filename
 end
 
-"Add a node to the dependency graph."
-function add_node!(G::SimpleDiGraph{Int64}, graph_data::DataFrame, filename::String; kwargs...)
+"""
+    addnode(plan::Plan, filename::String; kwargs...)
+
+Add a node to the dependency graph. Any key word arguments are added
+as properties to the node (and stored in plan.graph_data).
+"""
+function addnode!(plan::Plan, filename::String; kwargs...)
     # Make sure filename is not already in G:
-    @assert !(has_node(graph_data, filename))
+    @assert !(hasnode(plan, filename))
 
     # Update graph:
-    add_vertex!(G)
+    add_vertex!(plan.G)
 
     # Set properties;
     properties = Dict{Symbol, Any}(kwargs)
-    properties[:index] = nv(G)
+    properties[:index] = nv(plan.G)
     properties[:filename] = filename
 
     # Update data:
-    push!(graph_data, properties, cols=:union)
+    push!(plan.graph_data, properties, cols=:union)
 end
 
-function get_property(v::Int, graph_data::DataFrame, property::Symbol)
-    return filter(row -> row.index == v, graph_data)[!, property] |> only
+function getproperty(v::Int, plan::Plan, property::Symbol)
+    @assert v ∈ plan.graph_data.index
+    @assert string(property) ∈ names(plan.graph_data)
+    return filter(row -> row.index == v, plan.graph_data)[!, property] |> only
+end
+
+function getindex(value::Any, plan::Plan, property::Symbol)
+    @assert string(property) ∈ names(plan.graph_data)
+    return filter(row -> row[property] == value, plan.graph_data)[!, :index] |> only
 end
 
 """
-Run the plan defined by the dependency graph `G` and the associated information `graph_data`.
+    runplan(plan::Plan)
+
+Run the plan defined by the dependency graph `G` and the associated information
+ `graph_data`.
 
 ```julia
-G = SimpleDiGraph()
+plan = startplanning()
 
-add_target!(G, graph_data,
+addtarget!(plan,
            ["files/file_4.txt"], # output
            ["files/file_1.txt", "files/file_2"]) # depends on
 
-add_target!(G, graph_data,
+addtarget!(plan,
            ["files/file_5.txt"], # output
            ["files/file_4.txt", "files/file_3.txt"]) # depends on
 
-run_plan(G, graph_data)
+runplan(plan)
 ```
 """
-function run_plan(G::SimpleDiGraph, graph_data::DataFrame)
-    updates = get_update_schedule(G, graph_data)
-    for filename in updates[updates .!= nothing]
-        # Only run julia scripts. Data should be updated by scripts.
+function runplan(plan::Plan)
+    eval_order, is_stale = staletargets(plan)
+
+    while sum(is_stale) > 0
+        target = eval_order[is_stale] |> first
+        @info "Updating target: $target."
+        rundeps(target, plan)
+        eval_order, is_stale = staletargets(plan)
+    end
+    @info "The plan is up to date."
+end
+
+"""
+    uptodate(node::Int, plan::Plan)
+
+Check if a given node is newer than its immediate dependencies.
+"""
+function uptodate(node::Int, plan::Plan)
+    dependencies = inneighbors(plan.G, node)
+    # if node has no dependencies, it is always up to date
+    length(dependencies) == 0 && return true
+
+    dependency_change_times = getproperty.(dependencies, Ref(plan), :change_time)
+    if any(getproperty(node, plan, :change_time) .< dependency_change_times)
+        return false
+    else
+        return true
+    end
+end
+
+function updatechangetimes!(plan::Plan)
+    plan.graph_data[!, :change_time] = ctime.(plan.graph_data.filename)
+end
+
+"""
+    staletargets(plan::Plan)
+
+Get a tuple containing the evaluation order and which of these nodes are stale
+ (i.e. have dependencies newer than outputs).
+"""
+function staletargets(plan::Plan)
+    eval_order = topological_sort_by_dfs(plan.G)
+    updatechangetimes!(plan)
+    is_stale = .!uptodate.(eval_order, Ref(plan))
+    return eval_order, is_stale
+end
+
+"Run the dependencies for a given `node` in the `plan`."
+function rundeps(node::Int, plan::Plan)
+    dependencies = inneighbors(plan.G, node)
+    dependency_files = getproperty.(dependencies, Ref(plan), :filename)
+    # Only run julia scripts. Data should be updated by scripts.
+    for filename in dependency_files
         if last(filename, 3) == ".jl"
-            @info "Running file $(filename)."
+            @info "Running file: $(filename)."
             include(filename)
         end
     end
-end
-
-function get_update_schedule(G::SimpleDiGraph, graph_data::DataFrame)
-    evaluation_order = topological_sort_by_dfs(G)
-    update_change_times!(graph_data)
-    return check_dependencies.(evaluation_order, Ref(G), Ref(graph_data))
-end
-
-function check_dependencies(node::Int, G::SimpleDiGraph, graph_data::DataFrame)
-    dependencies = inneighbors(G, node)
-    # if node has no dependencies, go to next node
-    length(dependencies) == 0 && return nothing
-    dependency_change_times = get_property.(dependencies, Ref(graph_data), :change_time)
-    if any(get_property(node, graph_data, :change_time) .< dependency_change_times)
-        return get_property(node, graph_data, :filename)
-    else
-        return nothing
-    end
-end
-
-function update_change_times!(graph_data::DataFrame)
-    graph_data[!, :change_time] = ctime.(graph_data.filename)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,148 @@
 using Test, Planner
 
+# Test startplanning
+test_plan = startplanning()
+@test typeof(test_plan) == Plan
 
-out = plusTwo(3)
-@test out == 5
+@test typeof(test_plan.G) == SimpleDiGraph{Int64}
+@test nv(test_plan.G) == 0 && ne(test_plan.G) == 0
+
+@test typeof(test_plan.graph_data) == DataFrame
+@test size(test_plan.graph_data) == (0, 2)
+
+# TODO Test basic functionality:
+# add node
+tp = startplanning()
+addnode!(tp, "testnode1.csv")
+addnode!(tp, "testnode2.csv")
+@test nv(tp.G) == 2 && ne(tp.G) == 0
+@test size(tp.graph_data) == (2, 2)
+@test tp.graph_data.filename == ["testnode1.csv", "testnode2.csv"]
+@test tp.graph_data.index == [1, 2]
+@test_throws AssertionError addnode!(tp, "testnode2.csv")
+
+# has node
+tp = startplanning()
+addnode!(tp, "testnode1.csv")
+@test hasnode(tp, "testnode1.csv")
+@test !hasnode(tp, "testnode2.csv")
+
+# TODO add property
+
+# get property
+tp = startplanning()
+addnode!(tp, "testnode1.csv")
+@test getproperty(1, tp, :filename) == "testnode1.csv"
+@test_throws AssertionError getproperty(1, tp, :note) # property does not exist
+@test_throws AssertionError getproperty(2, tp, :filename) # node does not exit
+
+# add target
+tp = startplanning()
+addtarget!(tp,
+           ["someoutput.csv"],
+           ["someinput.txt", "somescript.jl"])
+
+@test nv(tp.G) == 3
+# make sure edges have been made
+@test collect(edges(tp.G)) == [Edge(1 => 3), Edge(2 => 3)]
+# make sure edges are between correct nodes
+@test getproperty.([1, 2, 3], Ref(tp), :filename) == ["someinput.txt", "somescript.jl", "someoutput.csv"]
+
+# change times and immediate dependencies
+tp = startplanning()
+# Create some test files:
+tempfiles = ["./testfile$i.txt" for i in 1:5]
+touch.(tempfiles)
+
+# Build graph:
+# 1------> 5
+# 2-->4--/
+# 3-/
+addtarget!(tp,
+           ["./testfile4.txt"],
+           ["./testfile2.txt", "./testfile3.txt"])
+
+addtarget!(tp,
+           ["./testfile5.txt"],
+           ["./testfile1.txt", "./testfile4.txt"])
+
+@test "change_time" ∉ tp.graph_data |> names
+update_change_times!(tp)
+@test "change_time" ∈ tp.graph_data |> names
+
+# Nothing should need to be updated.
+update_change_times!(tp)
+@test uptodate(get_index("./testfile4.txt", tp, :filename), tp)
+@test uptodate(get_index("./testfile5.txt", tp, :filename), tp)
+# Now testfile4 and testfile_5 should need to be updated:
+touch("./testfile1.txt")
+touch("./testfile2.txt")
+update_change_times!(tp)
+@test !uptodate(get_index("./testfile5.txt", tp, :filename), tp)
+@test !uptodate(get_index("./testfile4.txt", tp, :filename), tp)
+
+# get update_schedule:
+@test typeof(get_schedule(tp)) == Vector{Int}
+@test getproperty.(get_schedule(tp), Ref(tp), :filename) == ["./testfile4.txt", "./testfile5.txt"]
+new_plan = startplanning()
+@test staletargets(new_plan)|> length == 0
+
+# clean up:
+rm.(tempfiles)
+
+# run plan
+tp = startplanning()
+
+tempscripts = ["./t1.jl",
+               "./t2.jl",
+               "./t3.jl"]
+tempdata = ["./in1.csv",
+            "./in2.csv",
+            "./in3.csv"]
+
+touch.(vcat(tempdata, tempscripts))
+
+# Build graph:
+#      out2           out3
+#      /  | \          | \
+#    out1 |  \         |  \
+#  /  |   |   \        |   \
+# in1 t1 in2  t2      in3  t3
+
+
+addtarget!(tp,
+           ["./out1.csv"],
+           ["./t1.jl", "./in1.csv"])
+
+addtarget!(tp,
+           ["./out2.csv"],
+           ["./out1.csv", "./t2.jl", "./in2.csv"])
+
+addtarget!(tp,
+           ["./out3.csv"],
+           ["./in3.csv", "./t3.jl"])
+
+touch("./out1.csv")
+touch("./out2.csv")
+touch("./out3.csv")
+
+# Get all targets that are stale:
+# Get inputs for all of these targets:
+
+# run plan:
+# recursively get stale targets and evaluation order, run first stale targets dependencies.
+# this should fix the problem with non-connected graphs, and only stuff that needs to be run will.
+# given that runtime is much slower than planner time, this shouldnt matter.
+
+touch("./t1.jl")
+
+# This is the setup:
+eval_order, is_stale = staletargets(tp)
+# Get dependencies of first stale target:
+target = eval_order[is_stale] |> first
+dependencies = inneighbors(tp.G, target)
+dependency_files = getproperty.(dependencies, Ref(tp), :filename)
+# Run dependencies (that will re-create the new file.)
+touch.(dependency_files)
+touch.(getproperty(target, tp, :filename))
+
